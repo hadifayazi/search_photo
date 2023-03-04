a16858d75dc1ee0ebd6b254b4f62ce039455badc
hadifayazi <hadifayazi4@gmail.com>
2023-03-04 11:45:43 +0100

    updating get req with keyword for search

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (keyword) => {
   const res = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
     },
     params: {
-      query: "cars",
+      query: keyword,
     },
   });
   console.log(res);
